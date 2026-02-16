291c2e81cd850aac8714d6380bb412d57af9b72e
E1258763 Eduardo Regis Vieira <e1258763@interno.bb.com.br>
2026-02-16 02:39:08 -0300

    fix infra

--- infra/envs/dev/outputs.tf
+++ infra/envs/dev/outputs.tf
@@ -1,3 +1,5 @@
 output "ecr_repo_urls" { value = module.ecr.repo_urls }
 output "backup_bucket" { value = module.backups.bucket_name }
-output "ecs_cluster"   { value = module.ecs.cluster_name }
+output "ecs_cluster" { value = module.ecs.cluster_name }
+output "app_service" { value = module.ecs_services.app_service_name }
+output "minecraft_service" { value = module.ecs_services.minecraft_service_name }
--- infra/envs/dev/variables.tf
+++ infra/envs/dev/variables.tf
@@ -1,18 +1,59 @@
-variable "aws_region" { 
-    type = string 
-    default = "us-east-1"
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
 }
-variable "name"       { 
-    type = string 
-    default = "gamestack-dev"
+variable "name" {
+  type    = string
+  default = "gamestack-dev"
 }
 
 variable "environment" {
-    type = string
-    default = "desenv"
+  type    = string
+  default = "desenv"
 }
 
-variable "allowed_game_cidrs" { 
-    type = list(string) 
-    default = ["0.0.0.0/0"] 
+variable "allowed_game_cidrs" {
+  type    = list(string)
+  default = ["0.0.0.0/0"]
+}
+
+variable "allowed_api_cidrs" {
+  type    = list(string)
+  default = ["0.0.0.0/0"]
+}
+
+variable "router_port" {
+  type    = number
+  default = 80
+}
+
+variable "minecraft_host_port" {
+  type    = number
+  default = 25565
+}
+
+variable "minecraft_rcon_port" {
+  type    = number
+  default = 25575
+}
+
+variable "minecraft_loader" {
+  type    = string
+  default = "fabric"
+}
+
+variable "minecraft_version" {
+  type    = string
+  default = "1.21.1"
+}
+
+variable "minecraft_server_url" {
+  type    = string
+  default = ""
+}
+
+variable "minecraft_rcon_password" {
+  type      = string
+  sensitive = true
+  default   = "devpass"
 }
